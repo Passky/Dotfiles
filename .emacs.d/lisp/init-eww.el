62498f2f792973c5fc9118f1c50eb531249dc933
passky <cmpassky@outlook.com>
2021-04-13 03:33:12 +0800

    Update

--- .emacs.d/lisp/init-eww.el
+++ .emacs.d/lisp/init-eww.el
@@ -1,7 +1,5 @@
 (with-eval-after-load 'eww
-  (custom-set-variables
-   '(eww-search-prefix "https://www.bing.com/search?q=")
-   )
+  (setq eww-search-prefix "https://www.bing.com/search?q=-csdn.net+%s")
   ;; (define-key eww-mode-map (kbd "<C-i>")    'eww-forward-url)
   )
 (provide 'init-eww)
