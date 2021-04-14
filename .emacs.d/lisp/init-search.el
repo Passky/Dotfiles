5cbda495f5da2987e9241fb0c1956ed405d935b5
passky <cmpassky@outlook.com>
2021-04-14 15:07:56 +0800

    Update

--- .emacs.d/lisp/init-search.el
+++ .emacs.d/lisp/init-search.el
@@ -62,7 +62,13 @@
 ;; }}
 
 ;; {{ minibuffer completion
-(setq enable-recursive-minibuffers nil)     ; do not use minibuffer in minibuffer, causes bad bugs
+;; Do not allow the cursor in the minibuffer prompt
+(setq minibuffer-prompt-properties
+	  '(read-only t cursor-intangible t face minibuffer-prompt))
+(add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+;; Enable recursive minibuffers
+(setq enable-recursive-minibuffers t)
 (setq history-delete-duplicates t)          ; remove repeat history
 (define-key minibuffer-local-map (kbd "M-o") 'hydra-minibuffer/body)
 (define-key minibuffer-local-map (kbd "C-c C-o") 'embark-export)
