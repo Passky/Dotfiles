3230c37753fa8831ecd7eff3ade1f90a394f29e2
passky <cmpassky@outlook.com>
2021-04-24 13:56:25 +0800

    Update

--- .emacs.d/lisp/init-search.el
+++ .emacs.d/lisp/init-search.el
@@ -76,104 +76,110 @@
    :keymaps 'occur-mode-map
    "C-x C-o" 'occur-edit-mode)
 
-   (general-define-key
-	:keymaps 'isearch-mode-map
-	"M-<return>"                'isearch-repeat-forward
-	"M-S-<return>"              'isearch-repeat-backward
-	"C-o"                       'swiper-from-isearch
-	;; consistent with ivy-occur'
-	"C-c C-o"                   'isearch-occur ; consist with wgrep
-	;; Edit the search string instead of jumping back
-	[remap isearch-delete-char] 'isearch-del-char))
-  ;; }}
+  (general-define-key
+   :keymaps 'isearch-mode-map
+   "M-<return>"                'isearch-repeat-forward
+   "M-S-<return>"              'isearch-repeat-backward
+   "C-o"                       'swiper-from-isearch
+   ;; consistent with ivy-occur'
+   "C-c C-o"                   'isearch-occur ; consist with wgrep
+   ;; Edit the search string instead of jumping back
+   [remap isearch-delete-char] 'isearch-del-char))
+;; }}
 
-  ;; {{ minibuffer completion
-  ;; Do not allow the cursor in the minibuffer prompt
-  (setq minibuffer-prompt-properties
-		'(read-only t cursor-intangible t face minibuffer-prompt))
-  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+;; {{ minibuffer completion
+;; Do not allow the cursor in the minibuffer prompt
+(setq minibuffer-prompt-properties
+	  '(read-only t cursor-intangible t face minibuffer-prompt))
+(add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
 
-  ;; Enable recursive minibuffers
-  (setq enable-recursive-minibuffers t)
-  (setq history-delete-duplicates t)          ; remove repeat history
-  (define-key minibuffer-local-map (kbd "M-o") 'hydra-minibuffer/body)
-  (define-key minibuffer-local-map (kbd "C-c C-o") 'embark-export)
+;; Enable recursive minibuffers
+(setq enable-recursive-minibuffers t)
+(setq history-delete-duplicates t)          ; remove repeat history
+(define-key minibuffer-local-map (kbd "M-o") 'hydra-minibuffer/body)
+(define-key minibuffer-local-map (kbd "C-c C-o") 'embark-export)
 
-  ;; search actions
-  (my-add-package 'consult)
-  (after! consult
-	;; Or use `my-project-root'
-	(setq consult-project-root-function #'vc-root-dir)
-	(defun my-minibuffer-space ()
-	  (interactive)
-	  (if (and (string-prefix-p consult-async-default-split (minibuffer-contents))
-			   (= 2 (length (split-string (minibuffer-contents) consult-async-default-split))))
-		  (insert consult-async-default-split)
-		(when (looking-back consult-async-default-split) (delete-char -1))
-		(insert " ")))
-	(after! icomplete
-	  (define-key icomplete-minibuffer-map (kbd "SPC") 'my-minibuffer-space)))
-
-  (defun my-consult-grep ()
-	"Use ripgrep first then fallback to grep."
+;; search actions
+(my-add-package 'consult)
+(after! consult
+  ;; Or use `my-project-root'
+  (setq consult-project-root-function #'vc-root-dir
+		;; this will disable preview,unless you trigger it
+		consult-preview-key nil)
+  (defun my-minibuffer-space ()
 	(interactive)
-	(if
-		(executable-find "rg")
-		(call-interactively #'consult-ripgrep)
-	  (call-interactively #'consult-grep)))
+	(if (and (string-prefix-p consult-async-default-split (minibuffer-contents))
+			 (= 2 (length (split-string (minibuffer-contents) consult-async-default-split))))
+		(insert consult-async-default-split)
+	  (when (looking-back consult-async-default-split) (delete-char -1))
+	  (insert " ")))
+  (after! icomplete
+	(define-key icomplete-minibuffer-map (kbd "SPC") 'my-minibuffer-space))
 
-  (defun my-consult-grep-at-current-dir ()
-	"Use ripgrep first then fallback to grep,
+  ;; TODO: remove this in emacs 28
+  (setq xref-show-xrefs-function #'consult-xref
+		xref-show-definitions-function #'consult-xref))
+
+(defun my-consult-grep ()
+  "Use ripgrep first then fallback to grep."
+  (interactive)
+  (if
+	  (executable-find "rg")
+	  (call-interactively #'consult-ripgrep)
+	(call-interactively #'consult-grep)))
+
+(defun my-consult-grep-at-current-dir ()
+  "Use ripgrep first then fallback to grep,
 search in current directory."
-	(interactive)
-	(let ((consult-project-root-function nil))
-	  (if (executable-find "rg")
-		  (call-interactively #'consult-ripgrep default-directory)
-		(call-interactively #'consult-grep default-directory))))
+  (interactive)
+  (let ((consult-project-root-function nil))
+	(if (executable-find "rg")
+		(call-interactively #'consult-ripgrep default-directory)
+	  (call-interactively #'consult-grep default-directory))))
 
-  (my-add-package 'iedit) ; multi cursor
-  (with-eval-after-load 'iedit
-	(setq iedit-search-invisible nil))
-  ;; {{ embark
-  (my-add-package 'embark)
-  (my-add-package 'embark-consult)
-  (with-eval-after-load 'embark
-	(my-ensure 'embark-consult)
-	(general-define-key
-	 :keymaps 'embark-general-map
-	 "i" 'wgrep-change-to-wgrep-mode)
-	)
-  (add-hook #'embark-collect-mode #'embark-consult-preview-minor-mode)
+(my-add-package 'iedit) ; multi cursor
+(with-eval-after-load 'iedit
+  (setq iedit-search-invisible nil))
+;; {{ embark
+(my-add-package 'embark)
+(my-add-package 'embark-consult)
+(with-eval-after-load 'embark
+  (my-ensure 'embark-consult)
+  (general-define-key
+   :keymaps 'embark-general-map
+   "i" 'wgrep-change-to-wgrep-mode)
+  )
+(add-hook #'embark-collect-mode #'embark-consult-preview-minor-mode)
 
-  ;; from vmacs
-  (my-add-package 'orderless)
-  (my-delay-eval
-   #'(lambda ()
-	   (require 'orderless)))
+;; from vmacs
+(my-add-package 'orderless)
+(my-delay-eval
+ #'(lambda ()
+	 (require 'orderless)))
 
-  (after! orderless
-	(setq completion-styles (cons 'orderless completion-styles)) ;把orderless放到completion-styles 开头
-	;; 默认按空格开隔的每个关键字支持regexp/literal/initialism 3种算法
-	(setq orderless-matching-styles '(orderless-regexp orderless-literal orderless-initialism ))
-	(defun without-if-$! (pattern _index _total)
-	  (when (or (string-prefix-p "$" pattern) ;如果以! 或$ 开头，则表示否定，即不包含此关键字
-				(string-prefix-p "!" pattern))
-		`(orderless-without-literal . ,(substring pattern 1))))
-	(defun flex-if-comma (pattern _index _total) ;如果以逗号结尾，则以flex 算法匹配此组件
-	  (when (string-suffix-p "," pattern)
-		`(orderless-flex . ,(substring pattern 0 -1))))
-	(defun literal-if-= (pattern _index _total) ;如果以=结尾，则以literal  算法匹配此关键字
-	  (when (or (string-suffix-p "=" pattern)
-				(string-suffix-p "-" pattern)
-				(string-suffix-p ";" pattern))
-		`(orderless-literal . ,(substring pattern 0 -1))))
-	(setq orderless-style-dispatchers '(literal-if-= flex-if-comma without-if-$!)))
+(after! orderless
+  (setq completion-styles (cons 'orderless completion-styles)) ;把orderless放到completion-styles 开头
+  ;; 默认按空格开隔的每个关键字支持regexp/literal/initialism 3种算法
+  (setq orderless-matching-styles '(orderless-regexp orderless-literal orderless-initialism ))
+  (defun without-if-$! (pattern _index _total)
+	(when (or (string-prefix-p "$" pattern) ;如果以! 或$ 开头，则表示否定，即不包含此关键字
+			  (string-prefix-p "!" pattern))
+	  `(orderless-without-literal . ,(substring pattern 1))))
+  (defun flex-if-comma (pattern _index _total) ;如果以逗号结尾，则以flex 算法匹配此组件
+	(when (string-suffix-p "," pattern)
+	  `(orderless-flex . ,(substring pattern 0 -1))))
+  (defun literal-if-= (pattern _index _total) ;如果以=结尾，则以literal  算法匹配此关键字
+	(when (or (string-suffix-p "=" pattern)
+			  (string-suffix-p "-" pattern)
+			  (string-suffix-p ";" pattern))
+	  `(orderless-literal . ,(substring pattern 0 -1))))
+  (setq orderless-style-dispatchers '(literal-if-= flex-if-comma without-if-$!)))
 
-  ;; about configration see https://github.com/abo-abo/swiper/commit/28e88ab23a191420a93a4f920ca076674ee53f94
-  ;; "M-o"
-  (with-eval-after-load 'icomplete
-	(defhydra hydra-minibuffer (:hint nil :color pink)
-	  "
+;; about configration see https://github.com/abo-abo/swiper/commit/28e88ab23a191420a93a4f920ca076674ee53f94
+;; "M-o"
+(with-eval-after-load 'icomplete
+  (defhydra hydra-minibuffer (:hint nil :color pink)
+	"
 						^minibuffer action^
 --------------------------------------------------------------------------
 [_m_] mark            [_TAB_] Done        [_h_] Actions
@@ -183,27 +189,27 @@ search in current directory."
 [_q_] quit                                [<esc>] quit
 [_sf_] embark-act     [_ss_] embark-export 
 "
-	  ;; arrows
-	  ;; ("h" ivy-backward-delete-char)
-	  ("h" ivy-dispatching-done)
-	  ("gg" ivy-beginning-of-buffer)
-	  ("G" ivy-end-of-buffer)
-	  ("d" ivy-scroll-up-command)
-	  ("u" ivy-scroll-down-command)
-	  ;; actions
-	  ("q" keyboard-escape-quit :exit t)
-	  ("<escape>" keyboard-escape-quit :exit t)
-	  ("TAB" ivy-alt-done :exit nil)
-	  ("RET" ivy-done :exit t)
-	  ;; ("C-SPC" ivy-call-and-recenter :exit nil)
-	  ("f" ivy-call)
-	  ("c" ivy-toggle-calling)
-	  ("m" ivy-mark)
-	  ("M" ivy-unmark)
-	  ;; ("t" (setq truncate-lines (not truncate-lines)))
-	  ("sf" embark-act)
-	  ("ss" embark-export) 
-	  ("o" ivy-occur :exit t)))
+	;; arrows
+	;; ("h" ivy-backward-delete-char)
+	("h" ivy-dispatching-done)
+	("gg" ivy-beginning-of-buffer)
+	("G" ivy-end-of-buffer)
+	("d" ivy-scroll-up-command)
+	("u" ivy-scroll-down-command)
+	;; actions
+	("q" keyboard-escape-quit :exit t)
+	("<escape>" keyboard-escape-quit :exit t)
+	("TAB" ivy-alt-done :exit nil)
+	("RET" ivy-done :exit t)
+	;; ("C-SPC" ivy-call-and-recenter :exit nil)
+	("f" ivy-call)
+	("c" ivy-toggle-calling)
+	("m" ivy-mark)
+	("M" ivy-unmark)
+	;; ("t" (setq truncate-lines (not truncate-lines)))
+	("sf" embark-act)
+	("ss" embark-export) 
+	("o" ivy-occur :exit t)))
 
-  (provide 'init-search)
+(provide 'init-search)
 ;;; init-search.el ends here
