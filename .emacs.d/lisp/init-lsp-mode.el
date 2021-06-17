bdbda9eb1bd976e534672831acb6c2d2bc1f79c3
passky <cmpassky@outlook.com>
2021-06-17 13:38:39 +0800

    Update

--- .emacs.d/lisp/init-lsp-mode.el
+++ .emacs.d/lisp/init-lsp-mode.el
@@ -162,9 +162,9 @@
 		lsp-ui-doc-border (face-foreground 'default)
 		lsp-ui-doc-position 'at-point
 
-		lsp-ui-sideline-enable nil
-		lsp-ui-sideline-show-hover nil
-		lsp-ui-sideline-show-diagnostics nil
+		lsp-ui-sideline-enable t
+		lsp-ui-sideline-show-hover t
+		lsp-ui-sideline-show-diagnostics t
 		lsp-ui-sideline-show-code-actions t
 		lsp-ui-sideline-ignore-duplicate t
 
